f36531cbece257c2835df387a8a6c27714a56502
xavierDelaFuente <asnewyla@gmail.com>
2019-03-23 16:16:35 +0100

    feat:add routes and add players counter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import './Header.css';
 
-import logo from "../../data/img/Keyforge-calendar.png";
+import logo from '../../data/img/Keyforge-calendar.png';
 
 class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Keyforge. An App for easier keeping count of the game.</p>
-      </header>
-    );
-  }
+	render() {
+		const { className } = this.props;
+		return (
+			<header className={className || "App-header"}>
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Welcome to Keyforge. An App for easier keeping count of the game.</p>
+			</header>
+		);
+	}
 }
 
 export default Header;
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import App from "../App/App";
+import App from "../App/App.js";
+import Card from "../Card/Card.js";
+import Board from "../Board/Board.js";
 
 class Routes extends Component {
 	render() {
@@ -9,6 +11,8 @@ class Routes extends Component {
 			<Router>
 				<Switch>
 					<Route exact path="/" component={App} />
+					<Route exact path="/Board" component={Board} />
+					<Route exact path="/Card" component={Card} />
 				</Switch>
 			</Router>
 		);
